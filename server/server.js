9a86681d6832c424636e009189ff01a0cf092b81
Deven Pritchard <pritchad1@newpaltz.edu>
2026-05-11 20:22:24 -0400

    vite couildn't see ads.txt reconfigured server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,8 @@ const MIME_TYPES = {
   '.jpeg': 'image/jpeg',
   '.gif': 'image/gif',
   '.svg': 'image/svg+xml',
-  '.ico': 'image/x-icon'
+  '.ico': 'image/x-icon',
+  '.txt': 'text/plain; charset=UTF-8'
 };
 
 const server = http.createServer((req, res) => {
